097e776b0d6ab43593b71e25f129b2f63865ced1
Michał Hadamik <m.hadamik@codelabs.pl>
2021-04-19 15:37:24 +0200

    add vanilla js script for filtering

--- src/js/general/general.js
+++ src/js/general/general.js
@@ -1,23 +1,73 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function(event) { 
+    
+    let button = document.querySelectorAll(".btn");
+    let introBox = document.querySelectorAll(".intro__box");
 
-    var selectedtype="";
-  
-    $(".btn").click(function(){
-        $(this).addClass('btn-active').siblings().removeClass('btn-active');
-        selectedtype = $(this).attr('data-type');
-        if(selectedtype == "all"){
-            setTimeout(function(){
-                $(".intro__box").fadeIn();
-            },500);
+    let getAllSiblings = function (element) {
+        let siblings = [];
+        let sibling = element.parentNode.firstChild;
+        while (sibling) {
+            if (sibling.nodeType === 1 && sibling !== element) {
+                siblings.push(sibling);
+                sibling.classList.remove('btn-active');
+            }
+            sibling = sibling.nextSibling
         }
-        else {
-        $(".intro__boxes").fadeTo(100,0.1);
-        $(".intro__box").not("."+selectedtype).fadeOut();
-        setTimeout(function(){
-            $("."+selectedtype).fadeIn();
-            $(".intro__boxes").fadeTo(500,1);
-        },500);
-        }
+        return siblings;
+    };
+
+    let fade = function (element) {
+        let op = 1;
+        let timer = setInterval(function () {
+            if (op <= 0.1){
+                clearInterval(timer);
+                element.style.display = 'none';
+            }
+            element.style.opacity = op;
+            element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+            op -= op * 0.1;
+        }, 0);
+    }
+
+    let unfade = function (element) {
+        let op = 0.1; 
+        element.style.display = 'block';
+        let timer = setInterval(function () {
+            if (op >= 1){
+                clearInterval(timer);
+            }
+            element.style.opacity = op;
+            element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+            op += op * 0.1;
+        }, 0);
+    }
+
+    for (let i = 0 ; i < button.length; i++) {
+        button[i].addEventListener("click", function() {
+            let selectedtype = this.getAttribute('data-type');
+
+            this.classList.add("btn-active");
+
+            getAllSiblings(this);
+
+            if(selectedtype === "all") {
+                setTimeout(function(){
+                    for (let i = 0 ; i < introBox.length; i++) {
+                        unfade(introBox[i]);
+                    }
+                },500);
+            } 
+            else {
+                for (let i = 0 ; i < introBox.length; i++) {
+                    if( !(introBox[i].classList.contains(selectedtype)) ) {
+                        fade(introBox[i]);
+                    }
+                    else {
+                        unfade(introBox[i]);
+                    }
+                }
+            }
         });
-  });
-  
+    }
+
+});
